e9987d17cc3594504ed4848e0f3dbccbc409eecb
lonelydevBU <77091096+lonelydevBU@users.noreply.github.com>
2021-01-11 10:52:00 +0700

    Add files via upload

---
+++ Student's scoure.py
@@ -0,0 +1,18 @@
+a = 49.533
+b = 44.657
+c = 90.252
+d = 23
+f = 60.252
+
+xx = ("----- Your Score -----")
+cc = ("Foundation English : %.2f scores"%a)
+vv = ("General Business  : %.2f scores"%b)
+bb = ("Introduction to Computer Systems  : %.2f scores"%c)
+nn = ("Computer Programming : %.2f"%f)
+
+
+print(xx.center(50).upper())
+print(cc.center(50))
+print(vv.center(50))
+print(bb.center(50))
+print(nn.center(50))
