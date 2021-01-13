b7179465d0aac5b6c96e86949f5e7c4b49af6bb3
lonelydevBU <77091096+lonelydevBU@users.noreply.github.com>
2021-01-13 23:32:43 +0700

    Add files via upload

---
+++ Exercise5_1_Panuchai_J.py
@@ -0,0 +1,4 @@
+a = float(input("Enter your num : "))
+b = float(input("Enter your num : "))
+c1 = a + b;c2 = a - b;c3 = a * b;c4 = a / b
+print("%.2f + %.2f = %.2f\n"%(a,b,c1));print("%.2f - %.2f = %.2f\n"%(a,b,c2));print("%.2f * %.2f = %.2f\n"%(a,b,c3));print("%.2f / %.2f = %.2f\n"%(a,b,c4))
