bdc54b48cd6d9fb298602fd114982f7abb4fa943
lonelydevBU <77091096+lonelydevBU@users.noreply.github.com>
2021-01-13 23:43:39 +0700

    Add files via upload

---
+++ Exercise5_2_Panuchai_J.py
@@ -0,0 +1,6 @@
+s = float(input("Enter your distance(km) : "))
+t = float(input("Enter your time(hr) : "))
+v = s / t
+print("v = s / t")
+print("%.2f = %.2f / %.2f"%(v,s,t))
+print("Speed is : %.2f km/hr"%v)
